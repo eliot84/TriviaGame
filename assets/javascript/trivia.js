7f6a9f409ca87fb2ef63599da9d1afb05385ca3c
Eliot <mr.ear84@gmail.com>
2018-02-27 13:01:46 -0500

    Updating the project to include timer work, show and hide choice buttons, determining if a correct answer is chosen and added functions choices and choice listener

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -14,7 +14,21 @@ The Miami area was inhabited for thousands of years prior to any by this native 
 The Miccosukee, The Seminoles ,The Tequesta, The Taino
 The Tequesta
 */
-var timeLeft = 31;
+//Default State
+
+var triviaQuestions = [ ["Miami is the home to which National Park?", "The Everglades", "Monroe", "Cocoplum", "The SawGrass", "a1"], 
+						["In Miami Beach, more than 800 buildings feature this iconic style of architecture:", "Gothic", "Revivalism", "Art Deco", "Streamline Moderne"]
+					  ];
+
+var timeLeft = 15;
+
+	$(".start").show();
+    $("[value='a1']").hide();
+	$("[value='a2']").hide();
+	$("[value='a3']").hide();
+	$("[value='a4']").hide();
+	$(".question").hide();
+
 
 
 
@@ -23,8 +37,15 @@ $(".start").click(function(){
 });
 
 var start = function(){
-    console.log("hello!");
+    
+    //Set interface for Game
     $(".start").hide();
+    $("[value='a1']").show();
+	$("[value='a2']").show();
+	$("[value='a3']").show();
+	$("[value='a4']").show();
+	$(".question").show();
+
     game();
 };
 
@@ -32,15 +53,44 @@ var start = function(){
 var game = function(){
 
 	var timeLeft = setInterval(timer, 1000);
+    $(".question").text(triviaQuestions[0][0]);
+
+    choices();
+	choiceListener();
 };
 
 
-var timer = function(){
+
+var choices = function(){
+    $("[value='a1']").text(triviaQuestions[0][1]);
+	$("[value='a2']").text(triviaQuestions[0][2]);
+	$("[value='a3']").text(triviaQuestions[0][3]);
+	$("[value='a4']").text(triviaQuestions[0][4]);	
+}
+
+
+
+var choiceListener = function(){
+	$(".btn").click(function(){
+		console.log("you clicked me!");
+
+		if(this.value == triviaQuestions[0][5]){
+			console.log("CORRECT!");
+		}
+
+	});
+}
+
+
+
+var timer = function(stopTimer){
 
 	timeLeft -= 1;
 	
 	if(timeLeft > 0 )
 	{
 		document.getElementById("time").innerHTML = "Time Left is: " + timeLeft;
+	} else if(timeLeft == 0){
+		document.getElementById("time").innerHTML = "You ran out of time!";
 	}
 };
